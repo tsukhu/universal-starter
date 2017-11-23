e784b5397492ac7da14c4cafbcf8f826f5338ae5
Tarun Kumar Sukhu <tksukhu@gmail.com>
2017-11-23 16:05:59 +0530

    Added font loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as WebFont from 'webfontloader';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   cities = ['Paris', 'London', 'San Francisco'];
+
+  public ngOnInit() {
+    WebFont.load({
+      custom: {
+        families: ['Roboto',
+        'Omnes-ATT-W02'],
+        urls: [
+          'assets/styles/global/global.css'
+        ]
+      },
+      active: function() {
+        sessionStorage.fonts = true;
+      },
+      timeout: 2000
+    });
+  }
 }
