47779d0acb8b81d148315e212d0fe4e5346e74d5
Tarun Kumar Sukhu <tksukhu@gmail.com>
2017-11-24 08:20:22 +0530

    perf improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import * as WebFont from 'webfontloader';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  @Input() loading: boolean = false;
   cities = ['Paris', 'London', 'San Francisco'];
 
   public ngOnInit() {
