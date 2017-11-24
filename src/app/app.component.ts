aea807d6cc6f62aa29f5ede4ffed12167ffc1b8c
Tarun Kumar Sukhu <tksukhu@gmail.com>
2017-11-24 11:17:54 +0530

    compression added for ssr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
-import * as WebFont from 'webfontloader';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   @Input() loading: boolean = false;
   cities = ['Paris', 'London', 'San Francisco'];
-
-  public ngOnInit() {
-    WebFont.load({
-      custom: {
-        families: ['Helvetica',
-        'Omnes-ATT-W02']
-      },
-      active: function() {
-        sessionStorage.fonts = true;
-      },
-      timeout: 2000
-    });
-  }
 }
