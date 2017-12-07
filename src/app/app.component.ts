cc3f1d8b7525fc2b631a02b3b8af3b270f5774a6
Tarun Kumar Sukhu <tksukhu@gmail.com>
2017-12-07 17:15:03 +0530

    Fixes for router.events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { AppState } from './common/services/app.service';
 import { Router, NavigationStart } from '@angular/router';
 import { UnlockService } from './common/services/unlock.service';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-root',
@@ -16,11 +17,11 @@ export class AppComponent implements OnInit {
     private router: Router,
     private unlockService: UnlockService
   ) {
-  /*  router.events
-      .filter(event => event instanceof NavigationStart)
+    router.events
+      .filter((event) => event instanceof NavigationStart)
       .subscribe((event: NavigationStart) => {
         window.scrollTo(0, 0);
-      });*/
+      });
   }
 
   public ngOnInit() {
