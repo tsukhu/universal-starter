32f709ff6c72e950437b72acae995abb0edae627
Tarun Kumar Sukhu <tksukhu@gmail.com>
2017-12-11 10:42:14 +0530

    Increased timeout for karma

--- karma.conf.js
+++ karma.conf.js
@@ -28,6 +28,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: true
+    singleRun: true,
+    browserNoActivityTimeout: 60000
   });
 };
